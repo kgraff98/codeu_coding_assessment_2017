38f6c9ef5aa89f24538ed2d94598d6e6aef395d5
kgraff98 <kevinjgraff98@gmail.com>
2017-04-17 15:10:52 -0500

    Create Methods
    Created methods to obtain name and values for a JSON object. These
    helper methods assist in implementing the assigned methods.

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -17,66 +17,98 @@ package com.google.codeu.codingchallenge;
 import java.util.Collection;
 
 final class MyJSON implements JSON {
+
+  private String name;
+  private String val;
+  private JSON value;
+
+  public MyJSON(String name, JSON value) {
+    this.name = name;
+    this.value = value;
+  }
+  //get name of object
+  public String getName() {
+    return this.name;
+  }
+//----------------------------------------------------------------------------------------------
   // GET OBJECT
   // Get the value of the nested object with the given name. If there is
   // no nested object with that name, the method will return null.
   @Override
   public JSON getObject(String name) {
-    //Checks if name is null
+    //Check if name is null
     if(name == null) {
       return null;
     }
-    //Get the object value associated with the name
-    JSON json = this.get(name);
-    //Check if the object is null, throws exception otherwise
-    if(json == null) {
+
+    //Check if object name is equal to name
+    if(this.getName().equals(name)){
+      //Return object if True
+      return this;
+    }
+    else{
       return null;
     }
-    //Return the nested JSON object
-    return json;
   }
+//----------------------------------------------------------------------------------------------
   // SET OBJECT
   // Set the value of the nested object with the given name. Any old value
   // should be overwritten. This method will always return a reference to
   // "this".
   @Override
   public JSON setObject(String name, JSON value) {
-    //checks if name is null
-    if(name == null) {
-      return null;
-    }
-    //checks if value is null
-    if(value == null) {
-      return null;
-    }
-    this.opt(name) = value;
+    //Check if object name is equal to name
+    if(this.getName().equals(name)){
+      //Set value equal to new value
+      this.value = value;
+    //Always return reference to "this"
     return this;
   }
+//----------------------------------------------------------------------------------------------
   // GET STRING
   // Get the string value within this object that has the given name. if
   // there is no string with the given name, the method will return null.
   @Override
   public String getString(String name) {
-      JSON json = this.get(name);
-      if (json instanceof String) {
-        return (String) json;
-      }
-      else {
-        return null;
+    //Check if name is null
+    if(name == null) {
+      return null;
+    }
+
+    //Check if object name is equal to name
+    if(this.getName().equals(name)){
+      //Return string value if True
+      return this.val;
+    }
+    else {
+      return null;
     }
   }
-
+//----------------------------------------------------------------------------------------------
+  // SET STRING
+  // Set the string that should be stored under the given name. Any old value
+  // should be overwritten. This method will always return a reference to
+  // "this".
   @Override
   public JSON setString(String name, String value) {
-    // TODO: implement this
+    //Check if object name is equal to name
+    if(this.getName().equals(name)){
+      //Set string value equal to new value
+      this.val = value;
+    //Always return reference to "this"
     return this;
-  }
 
+//----------------------------------------------------------------------------------------------
+  // GET OBJECTS
+  //
+  // Copy the names of all object values to the given collection.
   @Override
   public void getObjects(Collection<String> names) {
     // TODO: implement this
   }
-
+  // GET STRINGS
+  //
+  // Copy the names of all string values to the given collection.
   @Override
   public void getStrings(Collection<String> names) {
     // TODO: implement this
